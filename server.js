c916c2442260640b800fcc700bb61af675fec7fc
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-05 12:57:09 -0400

    added server.js and created connection to express

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extend: false }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+//default response for any other request (Not Found).  must be last route.  will override others if on top
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
