e253d6a8fc9ec7e7f2e0463d6f786476212dfe50
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-05-05 13:21:57 -0400

    added database connection

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const mysql = require('mysql2');
+
 const express = require('express');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,14 +10,27 @@ const app = express();
 app.use(express.urlencoded({ extend: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: 'Lupin613!618',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
 
 
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 //default response for any other request (Not Found).  must be last route.  will override others if on top
